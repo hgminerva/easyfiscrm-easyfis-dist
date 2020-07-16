dd880b1a41270c309b8c8b2d572482fce6ff4fe2
Oliver Enciso <oliverenciso07@gmail.com>
2020-07-16 12:23:01 +0800

    Easyfis CRM

--- main.js
+++ main.js
@@ -289,13 +289,13 @@ var SoftwareRouteReuseStrategy = /** @class */ (function () {
     };
     SoftwareRouteReuseStrategy.prototype.store = function (route, handle) {
         switch (route.routeConfig.path) {
+            case 'setup/product/detail/:id': break;
             case 'trn/lead/detail/:id': break;
             case 'trn/sales/detail/:id': break;
             case 'trn/support/detail/:id': break;
             case 'trn/lead/:startDate/:endDate/:status/:userId/:dashboard': break;
             case 'trn/sales/:startDate/:endDate/:status/:userId/:dashboard': break;
             case 'trn/support/:startDate/:endDate/:status/:userId/:dashboard': break;
-            case 'setup/product/detail/:id': break;
             case 'setup/product': break;
             default: {
                 this.handlers[route.routeConfig.path] = handle;
